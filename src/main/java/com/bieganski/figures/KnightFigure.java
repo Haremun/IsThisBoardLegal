6632feea21f71efefbde63925585a918806706a3
Kamil <kamilbieg96@gmail.com>
2019-11-17 11:32:24 +0100

    temporary moveset for knight

--- src/main/java/com/bieganski/figures/KnightFigure.java
+++ src/main/java/com/bieganski/figures/KnightFigure.java
@@ -14,6 +14,14 @@ public class KnightFigure extends Figure {
     @Override
     public List<FieldCoordinates> checkCollision() {
         List<FieldCoordinates> coordinates = new ArrayList<>();
+        coordinates.add(new FieldCoordinates(getRow() - 2, getColumn() - 1)); //To think
+        coordinates.add(new FieldCoordinates(getRow() - 2, getColumn() + 1));
+        coordinates.add(new FieldCoordinates(getRow() + 2, getColumn() - 1));
+        coordinates.add(new FieldCoordinates(getRow() + 2, getColumn() + 1));
+        coordinates.add(new FieldCoordinates(getRow() - 1, getColumn() - 2));
+        coordinates.add(new FieldCoordinates(getRow() - 1, getColumn() + 2));
+        coordinates.add(new FieldCoordinates(getRow() + 1, getColumn() - 2));
+        coordinates.add(new FieldCoordinates(getRow() + 1, getColumn() + 2));
         return coordinates;
     }
 }
